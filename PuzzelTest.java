e8fe5942214a152eac07e0cef754abe3fae08cd0
mohammadbismail <m.basim.ismail@gmail.com>
2022-10-25 22:08:36 +0300

    Final without Bonus

--- PuzzelTest.java
+++ PuzzelTest.java
@@ -2,9 +2,11 @@ public class PuzzelTest {
     public static void main(String[] args) {
         PuzzleJava myPuzzel = new PuzzleJava();
 
-
-
-        //Testing
-        System.out.println(myPuzzel.getTenRolls());
+        // Testing
+        // System.out.println(myPuzzel.getTenRolls());
+        // System.out.println(myPuzzel.getRandomLetter());
+        System.out.println(myPuzzel.generatePassword());
+        System.out.println(myPuzzel.getNewPasswordSet(8));
     }
+
 }
--- PuzzleJava.java
+++ PuzzleJava.java
@@ -13,5 +13,32 @@ public class PuzzleJava {
         }
         return randArray;
     }
-    public ArrayList<Object> 
+
+    public Character getRandomLetter() {
+        ArrayList<Object> alphas = new ArrayList<Object>();
+        int randNumber = randMachine.nextInt(25);
+        char c;
+        for (c = 'A'; c < 'Z'; c++) {
+            alphas.add(c);
+        }
+        Character randomLetter = (Character) alphas.get(randNumber);
+        return randomLetter;
+    }
+
+    public String generatePassword() {
+        String password = "";
+        for (int i = 0; i < 8; i++) {
+            password += getRandomLetter();
+        }
+        return password;
+    }
+
+    public ArrayList<String> getNewPasswordSet(int arrLength) {
+        ArrayList<String> mySet = new ArrayList<String>();
+        for (int i = 0; i < arrLength; i++) {
+            mySet.add(generatePassword());
+        }
+        return mySet;
+    }
+
 }
